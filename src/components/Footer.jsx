cb740fc4ca4153e83dc27a8bebf3ac1fda521270
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2025-03-13 22:59:51 +0530

    Updated the Footer Section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,26 @@
 import ContentArea from "@/elements/ContentArea";
+import NavItem from "@/elements/NavItem";
+import {
+  IconBrandGithub,
+  IconBrandLinkedin,
+  IconMail,
+  IconPhone,
+} from "@tabler/icons-react";
 import React from "react";
 
 const Footer = () => {
   return (
     <section className="border-t-1 border-t-overlay">
       <ContentArea>
-        <footer className="flex items-center justify-between text-primary pt-10 font-semibold">
-          <p>cooders</p>
-          <p>Copyright</p>
+        <footer className="flex items-center justify-between pt-10 font-semibold">
+          <NavItem content="cooders" />
+          <p>Made with ❤️ &copy; 2025 COODERS</p>
+          <div className="flex items-center gap-3">
+            <IconBrandGithub className="hover:text-primary cursor-pointer" />
+            <IconBrandLinkedin className="hover:text-primary cursor-pointer" />
+            <IconMail className="hover:text-primary cursor-pointer" />
+            <IconPhone className="hover:text-primary cursor-pointer" />
+          </div>
         </footer>
       </ContentArea>
     </section>
